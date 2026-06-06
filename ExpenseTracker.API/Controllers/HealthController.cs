b75e370d473426a1bb1380b88a9e6e5687d6c49c
sambhu <smsam725@gmail.com>
2026-06-07 01:39:54 +0530

    initial commit

---
+++ ExpenseTracker.API/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "Expense Tracker API is running",
+                Time = DateTime.UtcNow
+            });
+   
+        }
+    }
+}
