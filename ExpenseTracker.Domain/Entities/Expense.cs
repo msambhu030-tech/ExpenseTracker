e4bd715d71a4230a9fcc709a311168edc8e54c51
sambhu <smsam725@gmail.com>
2026-06-07 15:04:17 +0530

    Added expenses

---
+++ ExpenseTracker.Domain/Entities/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseTracker.Domain.Entities;
+
+public class Expense
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public decimal Amount { get; set; }
+
+    public DateTime ExpenseDate { get; set; }
+}
---
+++ ExpenseTracker.Infrastructure/Data/ExpenseTrackerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Infrastructure.Data;
+
+public class ExpenseTrackerDbContext : DbContext
+{
+    public ExpenseTrackerDbContext(
+        DbContextOptions<ExpenseTrackerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Expense> Expenses => Set<Expense>();
+}
